250ee36ac07739a3380e1287ee4059b008ef8b26
hoonj170214 <sh20170214@gmail.com>
2023-08-18 11:02:52 +0900

    falsy

---
+++ falsy.js
@@ -0,0 +1,19 @@
+/*
+    JavaScript에서는 모든 값이 truthy / falsy 를 갖는다.
+    truthy : 참 같은 
+    falsy  : 거짓 같은
+    
+    truthy : falsy를 제외한 모든 값
+    falsy  : false, 0, "", NaN, null, undefined
+    
+    Boolean() 함수로 반환 여부 확인 가능
+
+    모든 문장에 논리 연산을 사용할 수 있다.
+    && (AND)     || (OR)      !(NOT)
+
+    Boolean("")     // false
+    Boolean(234)    // true
+    
+
+
+*/
