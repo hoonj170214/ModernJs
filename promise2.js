bebf23586b87790ac9c6bf2648f564e597f80aaf
hoonj170214 <sh20170214@gmail.com>
2023-08-18 14:22:29 +0900

    promise chaining

--- promise2.js
+++ promise2.js
@@ -39,6 +39,9 @@ const step3 = (message) => {
 
 // setTimeout  : 비동기
 
+// 비동기 함수를 순차대로 사용
+// promise Chaining
+// 축약하여 사용가능
 pizza()
   .then((result) => step1(result))
   .then((result) => step2(result))
@@ -46,3 +49,16 @@ pizza()
   .then(() => {
     console.log('피자가 준비되었습니다.');
   });
+
+/*
+  중복제거(축약) 가능(함수형 인터페이스)
+  .then 다음에 메소드명 만 집어넣어도 실행됨.
+  
+  pizza()
+  .then(step1)
+  .then(step2)
+  .then(step3)
+  .then(() => {
+    console.log('피자가 준비되었습니다.');
+  });
+*/
