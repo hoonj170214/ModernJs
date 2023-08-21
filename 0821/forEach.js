f33ecac55c6e63cebfb189885359ad11a71952e6
hoonj170214 <sh20170214@gmail.com>
2023-08-21 16:02:52 +0900

    배열 메서드 정리, 연습문제

--- 0821/forEach.js
+++ 0821/forEach.js
@@ -11,3 +11,19 @@ arr = [1, 2, 3, 4, 5];
 arr.forEach((item, idx) => {
   console.log(item, idx);
 });
+
+let arrPerson = [
+  { name: '홍길동' },
+  { name: '이몽룡' },
+  { name: '성춘향' },
+  { name: '변사또' },
+];
+
+//이름이 변사또인 사람의 인덱스를 찾아주세요
+let result = arrPerson.findIndex((item) => {
+  item.name === '변사또';
+});
+console.log(result);
+
+let arr2 = [1, 2, 3, 4, 5];
+const result2 = arr2.reduce((acc, item) => acc + item);
