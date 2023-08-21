723dd071d816d16973c49c2d78ee9cb5608f78ca
hoonj170214 <sh20170214@gmail.com>
2023-08-21 11:48:07 +0900

    복습

--- 0821/review.js
+++ 0821/review.js
@@ -13,4 +13,7 @@ let result = function 홀짝(number) {
   number % 2 === 1 ? console.log('홀수 입니다.') : console.log('짝수 입니다.');
 };
 
-홀짝(number);
+result(number);
+
+// const 로 객체를 설정해도, 프로퍼티는 변경 조작 가능하다.
+// const person ={};    는 불가능
